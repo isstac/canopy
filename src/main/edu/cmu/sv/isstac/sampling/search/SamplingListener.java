1b8d141a10c42e35b5153f1390abf5db9280a3e9
Kasper Luckow <ksluckow@gmail.com>
2016-09-23 17:22:38 -0700

    Begin refactoring sampling listeners to harness commonalities

--- src/main/edu/cmu/sv/isstac/sampling/search/SamplingListener.java
+++ src/main/edu/cmu/sv/isstac/sampling/search/SamplingListener.java
@@ -1,7 +1,9 @@
 package edu.cmu.sv.isstac.sampling.search;
 
+import gov.nasa.jpf.PropertyListenerAdapter;
+
 /**
  * @author Kasper Luckow
  */
-public class SamplingListener {
+public class SamplingListener extends PropertyListenerAdapter {
 }
