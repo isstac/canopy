429d32379886e3d6b3dafdb58d25dacc0aedae77
Kasper Luckow <ksluckow@gmail.com>
2016-10-28 10:52:23 -0700

    Add exhaustive strat and experiment

---
+++ src/main/edu/cmu/sv/isstac/sampling/batch/ExhaustiveExperiment.java
@@ -0,0 +1,27 @@
+package edu.cmu.sv.isstac.sampling.batch;
+
+import edu.cmu.sv.isstac.sampling.AnalysisStrategy;
+import edu.cmu.sv.isstac.sampling.Options;
+import edu.cmu.sv.isstac.sampling.exhaustive.ExhaustiveStrategy;
+import edu.cmu.sv.isstac.sampling.exploration.AllChoicesStrategy;
+import edu.cmu.sv.isstac.sampling.quantification.ModelCounterCreationException;
+import edu.cmu.sv.isstac.sampling.quantification.ModelCounterFactory;
+import edu.cmu.sv.isstac.sampling.quantification.SPFModelCounter;
+import edu.cmu.sv.isstac.sampling.reinforcement.ReinforcementLearningStrategy;
+import gov.nasa.jpf.Config;
+
+/**
+ * @author Kasper Luckow
+ */
+public class ExhaustiveExperiment implements Experiment {
+
+  @Override
+  public AnalysisStrategy createAnalysisStrategy(Config config, int seed) {
+    return new ExhaustiveStrategy();
+  }
+
+  @Override
+  public String getName() {
+    return "Exhaustive";
+  }
+}
