7f32ca4a850d41de1a7ac0b8a06eb2e3dc3a5876
Kasper Luckow <ksluckow@gmail.com>
2016-10-28 15:46:02 -0700

    Update batch experiment for reinforcement learning

--- src/main/edu/cmu/sv/isstac/sampling/batch/RLExperiment.java
+++ src/main/edu/cmu/sv/isstac/sampling/batch/RLExperiment.java
@@ -3,12 +3,14 @@ package edu.cmu.sv.isstac.sampling.batch;
 import edu.cmu.sv.isstac.sampling.AnalysisStrategy;
 import edu.cmu.sv.isstac.sampling.Options;
 import edu.cmu.sv.isstac.sampling.exploration.AllChoicesStrategy;
-import edu.cmu.sv.isstac.sampling.policies.SimulationPolicy;
 import edu.cmu.sv.isstac.sampling.quantification.ModelCounterCreationException;
 import edu.cmu.sv.isstac.sampling.quantification.ModelCounterFactory;
 import edu.cmu.sv.isstac.sampling.quantification.SPFModelCounter;
+import edu.cmu.sv.isstac.sampling.reinforcement.RLNode;
+import edu.cmu.sv.isstac.sampling.reinforcement.RLNodeFactory;
+import edu.cmu.sv.isstac.sampling.reinforcement.RLNodeFactoryMCDecorator;
 import edu.cmu.sv.isstac.sampling.reinforcement.ReinforcementLearningStrategy;
-import edu.cmu.sv.isstac.sampling.reinforcement.Utils;
+import edu.cmu.sv.isstac.sampling.structure.NodeFactory;
 import gov.nasa.jpf.Config;
 
 /**
@@ -18,14 +20,17 @@ public class RLExperiment implements Experiment {
 
   private final boolean pruning;
   private final boolean rewardAmplifcation;
+  private final boolean modelCountSubDomain;
   private final int samplesPerOptimization;
   private final double epsilon;
   private final double historyWeight;
 
-  public RLExperiment(boolean pruning, boolean rewardAmplification, int samplesPerOptimization,
+  public RLExperiment(boolean pruning, boolean rewardAmplification, boolean modelCountSubDomain,
+                      int samplesPerOptimization,
                       double epsilon, double historyWeight) {
     this.pruning = pruning;
     this.rewardAmplifcation = rewardAmplification;
+    this.modelCountSubDomain = modelCountSubDomain;
     this.samplesPerOptimization = samplesPerOptimization;
     this.epsilon = epsilon;
     this.historyWeight = historyWeight;
@@ -41,13 +46,22 @@ public class RLExperiment implements Experiment {
         Boolean.toString(this.rewardAmplifcation));
     config.setProperty(Options.RNG_SEED, Integer.toString(seed));
 
-    try {
-      SPFModelCounter modelCounter = ModelCounterFactory.getInstance(config);
-      return new ReinforcementLearningStrategy(samplesPerOptimization, epsilon, historyWeight,
-          modelCounter, seed);
-    } catch (ModelCounterCreationException e) {
-      throw new BatchProcessorException(e);
+    NodeFactory<RLNode> factory;
+
+    if(modelCountSubDomain) {
+      try {
+        SPFModelCounter modelCounter = ModelCounterFactory.getInstance(config);
+        factory = new RLNodeFactoryMCDecorator(modelCounter);
+      } catch (ModelCounterCreationException e) {
+        throw new BatchProcessorException(e);
+      }
+    } else {
+      factory = new RLNodeFactory();
     }
+
+    return new ReinforcementLearningStrategy(samplesPerOptimization, epsilon, historyWeight,
+        factory, seed);
+
   }
 
   @Override
@@ -55,6 +69,7 @@ public class RLExperiment implements Experiment {
     return "RL[" +
         "pruning=" + this.pruning + ";" +
         "rewardAmp=" + this.rewardAmplifcation + ";" +
+        "modelCountSubDomain=" + this.modelCountSubDomain + ";" +
         "samplesPerOptimization=" + this.samplesPerOptimization + ";" +
         "epsilon=" + this.epsilon + ";" +
         "history=" + this.historyWeight + "]";
