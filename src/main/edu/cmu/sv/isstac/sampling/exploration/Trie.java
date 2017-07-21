72e193556dfe927c1b9d073d37638b8124adf2e2
Kasper Luckow <ksluckow@gmail.com>
2017-07-20 17:07:15 -0700

    Update license headers to MIT

--- src/main/edu/cmu/sv/isstac/sampling/exploration/Trie.java
+++ src/main/edu/cmu/sv/isstac/sampling/exploration/Trie.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2017 The ISSTAC Authors
+ * Copyright (c) 2017 Carnegie Mellon University.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
